870f02f646368fcfe13970f74bf32bc4e1f07c32
icodeunot <jsal.dev@protonmail.com>
2026-01-31 01:54:05 -0800

    Added HTMX edit

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_browser_reload",
+    "crispy_forms",
+    "crispy_bootstrap4",
     "party",
 ]
 
@@ -127,3 +129,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Custum User Auth model
 AUTH_USER_MODEL = "party.CustomUser"
+
+# Crispy Form settings
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
